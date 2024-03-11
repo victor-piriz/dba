73d0ae3dac628d5a1ad17d40adf8bf52c611dcb0
Victor <131294248+victor-piriz@users.noreply.github.com>
2024-03-11 19:22:11 -0300

    Update KILL POR USER.sql

--- MySQL/KILL POR USER.sql
+++ MySQL/KILL POR USER.sql
@@ -1,6 +1,6 @@
-MYSQL_USER=admin
-MYSQL_ENDPOINT=eu1-payments-sbox-sandboxdb.ciy3udq0jwbu.eu-west-1.rds.amazonaws.com
-USER_TO_KILL=glopez
+MYSQL_USER=
+MYSQL_ENDPOINT=
+USER_TO_KILL=
 FILE=kill_$USER_TO_KILL
 
 MYSQL_CONN="-h ${MYSQL_ENDPOINT} -u${MYSQL_USER} -p"
