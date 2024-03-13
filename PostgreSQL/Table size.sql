e55a6bc3e7ad830f6e5631dc10772ff445463432
victor-piriz <piriz.victor@gmail.com>
2024-03-13 10:59:07 -0300

    postgresql_table_size

--- PostgreSQL/Table size.sql
+++ PostgreSQL/Table size.sql
@@ -1,13 +1,12 @@
 SELECT
     pg_statio_user_tables.relname AS table_name,
-    pg_size_pretty(pg_total_relation_size(pg_statio_user_tables.relid)) AS total_size,
-    pg_size_pretty(pg_relation_size(pg_statio_user_tables.relid)) AS data_size,
-    pg_size_pretty(pg_total_relation_size(pg_statio_user_tables.relid) - pg_relation_size(pg_statio_user_tables.relid)) AS index_size,
-    pg_class.reltuples AS row_count
+    pg_total_relation_size(pg_statio_user_tables.relid) / (1024 * 1024) AS total_size_mb,
+    pg_relation_size(pg_statio_user_tables.relid) / (1024 * 1024) AS data_size_mb,
+    (pg_total_relation_size(pg_statio_user_tables.relid) - pg_relation_size(pg_statio_user_tables.relid)) / (1024 * 1024) AS index_size_mb,
+    to_char(pg_class.reltuples, '999,999,999') AS row_count
 FROM
     pg_catalog.pg_statio_user_tables
 JOIN
     pg_class ON pg_statio_user_tables.relid = pg_class.oid
 ORDER BY
-    pg_total_relation_size(pg_statio_user_tables.relid) DESC
-LIMIT 10;
+    pg_total_relation_size(pg_statio_user_tables.relid) DESC;
